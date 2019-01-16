493093e0a4bce62a077e09273e7cb7f572af8c7b
Juba <rgjuba@gmail.com>
2019-01-16 07:22:56 -0200

    token verify

--- jwt-express-mongodb/routes/auth.js
+++ jwt-express-mongodb/routes/auth.js
@@ -21,11 +21,11 @@ router.post('/login', (req, res, next) => {
 
         //find user in MongoDB
         const handler = (err, result) => {
-            if (!err && bcrypt.compareSync(password, result.password)) {
+            if (!err && result !== null && bcrypt.compareSync(password, result.password)) {
                 let tokenData = {
                     name: result.name,
                     email: result.email
-                };
+                }
                 let generatadeToken = jwt.sign(tokenData, config.JWT_KEY, { expiresIn: '1m' });
                 res.json({
                     success: true,
@@ -34,8 +34,8 @@ router.post('/login', (req, res, next) => {
             } else {
                 res.status(401).json({
                     success: false,
-                    code: 'Error_02',
-                    message: err
+                    code: 'API_Error_02',
+                    message: err || 'User does not exists'
                 })
             }
         }
@@ -43,6 +43,25 @@ router.post('/login', (req, res, next) => {
         db.findUser({ email }, handler);
     }
 
+});
+
+router.get('/verifytoken', (req, res, next) => {
+    //[0] = Bearer , [1] = Token
+    let token = req.headers['authorization'].split(' ')[1];
+    jwt.verify(token,config.JWT_KEY,(err,decode)=>{
+        if(!err){
+            res.json({
+                success: true,
+                message: 'Token is valid'
+            });
+        }else{
+            res.status(401).json({
+                success: false,
+                message: 'Token is not valid',
+                error: err
+            });
+        }
+    });
 });
 
 module.exports = router;
