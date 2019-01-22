7fe7db786fe147d7e4d8386a9e0c5d1c60f7c9ca
Juba <rgjuba@gmail.com>
2019-01-22 05:52:53 -0200

    list users authenticated

--- web-react-jwt/src/ui/LoginForm.js
+++ web-react-jwt/src/ui/LoginForm.js
@@ -18,7 +18,9 @@ class LoginForm extends Component {
                 success: undefined,
                 message: undefined
             },
-            logged: false
+            logged: false,
+            users: undefined,
+            error: undefined
         }
     }
 
@@ -33,7 +35,43 @@ class LoginForm extends Component {
     verifytoken() {
         let url = 'http://localhost:3001/auth/verifytoken';
         let token = localStorage.getItem('TEST_TOKEN');
-        if(!token){
+        if (!token) {
+            this.setState({
+                error: "No token defined. Please Login"
+            })
+            return
+        }
+
+        fetch(url, {
+            method: "GET",
+            body: undefined,
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `Bearer ${token}`
+            },
+        }).then(response => response.json())
+            .then(responseJson => {
+                if (responseJson.success) {
+                    this.setState({
+                        logged: responseJson.success,
+                        error: undefined
+                    })
+                    this.loadUsers()
+                } else {
+                    this.setState({
+                        error: responseJson.error.message
+                    })
+                }
+            }).catch(err => this.setState({ error: err }))
+    }
+
+    loadUsers() {
+        let url = 'http://localhost:3001/users/listusers';
+        let token = localStorage.getItem('TEST_TOKEN');
+        if (!token) {
+            this.setState({
+                error: "No token defined. Please Login"
+            })
             return
         }
 
@@ -47,10 +85,12 @@ class LoginForm extends Component {
         }).then(response => response.json())
             .then(responseJson => {
                 this.setState({
-                    logged : responseJson.success
+                    users: responseJson.data,
+                    error: undefined
                 })
-            }).catch(err => console.log('Error ', err))
+            }).catch(err => this.setState({ error: err }))
     }
+
 
     showAuthorizedArea() {
         if (this.state.logged) {
@@ -136,10 +176,15 @@ class LoginForm extends Component {
                 if (responseJson.success) {
                     localStorage.setItem('TEST_TOKEN', responseJson.token);
                     this.setState({
-                        logged: true
+                        logged: true,
+                        error: undefined,
+
                     });
+                    this.loadUsers()
                 }
-            });
+            }).catch(err => console.log('Error ', err));
+
+            e.target.reset()
     }
 
     handleEmailChange(e) {
@@ -205,7 +250,7 @@ class LoginForm extends Component {
                         </div>
                     </div>
                 </div>
-                {/* Begin Modal Register Form */}
+                {/* End Modal Register Form */}
 
                 {/* Begin Modal List Authenticad List  */}
 
@@ -219,6 +264,24 @@ class LoginForm extends Component {
 
                                 <div className="list-group">
 
+                                    {
+                                        /**
+                                         * List the users when authenticated
+                                         */
+                                        (this.state.users !== undefined && this.error === undefined) ? (
+                                            this.state.users.map((user) => (
+                                                <a key={user.email} href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                                                    <div className="d-flex w-100 justify-content-between">
+                                                        <h5 className="mb-1">Name: {user.name}</h5>
+                                                        <small>01/09/2017</small>
+                                                    </div>
+                                                    <p className="mb-1">E-mail: {user.email}</p>
+                                                    <small>Password (Should never do that)</small>
+                                                </a>
+                                            ))
+                                        ) : this.state.error
+
+                                    }
 
                                 </div>
 
